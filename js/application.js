76482c0db1abfcb06bcbac40956be8e8f3a01561
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2017-11-23 20:07:26 +0700

    Represent the default pendulum's options at startup

--- js/application.js
+++ js/application.js
@@ -8,48 +8,63 @@ define(['mdc', 'canvasState'], function (mdc, CanvasState) {
             const MDCTextField = mdc.textField.MDCTextField;
 
             // Инициализируем поля ввода MDC
-            const inputFields = Array.from(document.querySelectorAll(".mdc-text-field"));
-            inputFields.forEach((inputField) => {
+            this.inputFields = Array.from(document.querySelectorAll(".mdc-text-field"));
+            this.inputFields.forEach((inputField) => {
                 new MDCTextField(inputField);
             });
 
             // Инициализируем обычные поля ввода
-            const lengthEl = document.querySelector("#length");
-            const decelerationEl = document.querySelector("#deceleration");
+            this.lengthEl = document.querySelector("#length");
+            this.decelerationEl = document.querySelector("#deceleration");
+
+            // Заполняем поля в зависимости от стандартных значений маятника
+            this.lengthEl.value = this.canvasState.defaultPendulumOptions.length;
+            this.decelerationEl.value = this.canvasState.defaultPendulumOptions.deceleration;
 
             // Инициализируем переключатель
-            const toggle = document.querySelector("#switch");
+            this.toggleEl = document.querySelector("#switch");
+
+            // Устанавливаем положение переключателя в зависимости от стандартных значений маятника
+            this.toggleEl.checked = this.canvasState.defaultPendulumOptions.run;
+
+            // Обрабатываем случай, если маятник был запущен при запуске приложения
+            this.toggleChangeHandler();
 
             // Регистрируем обработчики событий для полей и переключателя
-            lengthEl.addEventListener("change", () => {
-                if (lengthEl.checkValidity()) {
-                    this.canvasState.updatePendulumData(null, parseFloat(lengthEl.value), null);
+            this.lengthEl.addEventListener("change", () => {
+                if (this.lengthEl.checkValidity()) {
+                    this.canvasState.updatePendulumData(null, parseFloat(this.lengthEl.value), null);
                 }
             });
 
-            decelerationEl.addEventListener("change", () => {
-                if (decelerationEl.checkValidity()) {
-                    this.canvasState.updatePendulumData(null, null, parseFloat(decelerationEl.value));
+            this.decelerationEl.addEventListener("change", () => {
+                if (this.decelerationEl.checkValidity()) {
+                    this.canvasState.updatePendulumData(null, null, parseFloat(this.decelerationEl.value));
                 }
             });
 
-            toggle.addEventListener('change', () => {
-                this.canvasState.pendulum.run = toggle.checked;
-                this.canvasState.valid = false;
+            this.toggleEl.addEventListener('change', () => this.toggleChangeHandler());
+        }
 
-                // Блокируем или разблокируем каждое поле ввода, в зависимости от положения переключателя
-                lengthEl.disabled = !lengthEl.disabled;
-                decelerationEl.disabled = !decelerationEl.disabled;
+        toggleChangeHandler() {
+            this.canvasState.pendulum.run = this.toggleEl.checked;
+            this.canvasState.valid = false;
 
-                inputFields.forEach((inputField => {
-                    if (toggle.checked) {
-                        inputField.classList.add("mdc-text-field--disabled");
-                    }
-                    else {
-                        inputField.classList.remove("mdc-text-field--disabled");
-                    }
-                }));
-            });
+            // Блокируем или разблокируем каждое поле ввода, в зависимости от положения переключателя
+            this.inputFields.forEach((inputField => {
+                if (this.toggleEl.checked) {
+                    this.lengthEl.disabled = true;
+                    this.decelerationEl.disabled = true;
+
+                    inputField.classList.add("mdc-text-field--disabled");
+                }
+                else {
+                    this.lengthEl.disabled = false;
+                    this.decelerationEl.disabled = false;
+
+                    inputField.classList.remove("mdc-text-field--disabled");
+                }
+            }));
         }
     }
 
